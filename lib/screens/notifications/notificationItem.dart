020eb468d09896dd8532e9852f7cdc96f3a6c8c9
manhphung7122 <manhphung7122@gmail.com>
2023-02-12 02:05:31 +0700

    noti and account

---
+++ lib/screens/notifications/notificationItem.dart
@@ -0,0 +1,13 @@
+class NotificationItem {
+  final String title;
+  final String detail;
+
+  NotificationItem(this.title, this.detail);
+}
+
+List<NotificationItem> notiItem = [
+  NotificationItem("Notification 1", "Detail 1"),
+  NotificationItem("Notification 2", "Detail 2"),
+  NotificationItem("Notification 3", "Detail 3"),
+];
+
