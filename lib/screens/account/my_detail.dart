68b2fe11d940e566249f8e39d9b050435d28ac56
manhphung7122 <manhphung7122@gmail.com>
2023-02-19 23:59:15 +0700

    small things

---
+++ lib/screens/account/my_detail.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/widgets.dart';
+
+class MyDetail extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text()
+            ],
+          )
+        ),
+      )
+    );
+  }
+}
