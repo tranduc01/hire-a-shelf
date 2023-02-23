3779a232f334f353962bf0358af535c73e216e4c
manhphung7122 <manhphung7122@gmail.com>
2023-02-24 02:57:18 +0700

    unused element

--- lib/screens/account/account_item.dart
+++ lib/screens/account/account_item.dart
@@ -7,14 +7,14 @@ import 'Help/help.dart';
 class AccountItem {
   final String label;
   final String iconPath;
-  final int index;
+
   final Widget screen;
 
-  AccountItem(this.label, this.iconPath, this.index,this.screen);
+  AccountItem(this.label, this.iconPath, this.screen);
 }
 
 List<AccountItem> accountItems = [
-  AccountItem("My Details", "assets/icons/account_icons/details_icon.svg", 0, MyDetail()),
-  AccountItem("Help", "assets/icons/account_icons/help_icon.svg", 1, Help()),
-  AccountItem("About", "assets/icons/account_icons/about_icon.svg", 2, About()),
+  AccountItem("My Details", "assets/icons/account_icons/details_icon.svg", MyDetail()),
+  AccountItem("Help", "assets/icons/account_icons/help_icon.svg", Help()),
+  AccountItem("About", "assets/icons/account_icons/about_icon.svg", About()),
 ];
