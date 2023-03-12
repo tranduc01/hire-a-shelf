83738d5aed3ef975fbf64c7f7f981fb628b8ceb8
manhphung7122 <manhphung7122@gmail.com>
2023-03-12 22:56:17 +0700

    add icon

--- lib/screens/account/Help/help.dart
+++ lib/screens/account/Help/help.dart
@@ -9,54 +9,52 @@ class Help extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Container(
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 10,
-              ),
-              Align(
-                alignment: Alignment.topLeft,
-                child: BackButton(),
-              ),
-              homeScreenIcon(),
-              SizedBox(
-                height: 10,
-              ),
-              ListTile(
-                title: AppText(
-                  text: "Help",
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  textAlign: TextAlign.center,
-                ),
-                subtitle: AppText(
-                  text:
-                      'IF YOU HAD ANY PROBLEMS, PLEASE CONTACT WITHIN THE INFORMATION BELOW.',
-                  textAlign: TextAlign.center,
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Column(
-                children: getChildrenWithSeperator(
-                  widgets: helpItems.map((e) {
-                    return getHelpItemWidget(e);
-                  }).toList(),
-                  seperator: Divider(
-                    thickness: 1,
+          child: Container(
+              child : SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: BackButton(),
+                    ),
+                    homeScreenIcon(),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    ListTile(
+                      title: AppText(
+                        text: "Help",
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        textAlign: TextAlign.center,
+                      ),
+                      subtitle: AppText(
+                        text:
+                            'IF YOU HAD ANY PROBLEMS, PLEASE CONTACT WITHIN THE INFORMATION BELOW.',
+                        textAlign: TextAlign.center,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Column(
+                      children: getChildrenWithSeperator(
+                        widgets: helpItems.map((e) {
+                          return getHelpItemWidget(e);
+                        }).toList(),
+                        seperator: Divider(
+                          thickness: 1,
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
-            ],
           ),
         ),
-      ),
-    ));
+      )
+    );
   }
 
   Widget homeScreenIcon() {
@@ -71,23 +69,29 @@ class Help extends StatelessWidget {
   Widget getHelpItemWidget(HelpItem helpItem) {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 15),
-      padding: EdgeInsets.symmetric(horizontal: 25),
+      padding: EdgeInsets.symmetric(horizontal: 35),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          SvgPicture.asset("assets/icons/account_icons/phone_icon.svg",width: 21, height: 21),
           SizedBox(
-            width: 40,
-            height: 10,
+            width: 5,
           ),
           Text(
-            helpItem.tel,
-            style: TextStyle(fontSize: 18),
+            ": " + helpItem.tel,
+            style: TextStyle(fontSize: 18)
           ),
           SizedBox(
-            width: 50,
+            width: 30,
+          ),
+          SvgPicture.asset("assets/icons/account_icons/email_icon.svg",width: 21, height: 21),
+          SizedBox(
+            width: 5,
           ),
           Text(
-            helpItem.email,
-            style: TextStyle(fontSize: 18),
+            ": " + helpItem.email,
+            style: TextStyle(fontSize: 18)
           ),
         ],
       ),
