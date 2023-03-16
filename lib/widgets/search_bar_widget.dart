cbe3d3bf2e64b3d171ba254fda12e1246ab3a019
tranduc01 <doantranduc01@gmail.com>
2023-03-16 14:52:42 +0700

    add location detector

--- lib/widgets/search_bar_widget.dart
+++ lib/widgets/search_bar_widget.dart
@@ -1,8 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 
-class SearchBarWidget extends StatelessWidget {
-  final String searchIcon = "assets/icons/search_icon.svg";
+class SearchBarWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBarWidget> {
+  String location = 'Null, Press Button';
+  String address = 'Location Here';
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentLocation();
+  }
+
+  Future<Position> _getGeoLocationPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      await Geolocator.openLocationSettings();
+      return Future.error('Location services are disabled.');
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+
+  Future<void> getAddressFromLatLong(Position position) async {
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+    print(position);
+    print(placemarks);
+    Placemark place = placemarks[0];
+    address =
+        '${place.street}, ${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}';
+    setState(() {});
+  }
+
+  getCurrentLocation() async {
+    Position position = await _getGeoLocationPosition();
+    getAddressFromLatLong(position);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,19 +80,18 @@ class SearchBarWidget extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          SvgPicture.asset(
-            searchIcon,
-          ),
+          Icon(Icons.location_searching_rounded),
           SizedBox(
             width: 8,
           ),
-          Text(
-            "Search Here",
+          Expanded(
+              child: Text(
+            "$address",
             style: TextStyle(
                 fontSize: 18,
                 fontWeight: FontWeight.bold,
                 color: Color(0xFF7C7C7C)),
-          )
+          ))
         ],
       ),
     );
